031c47a257df2f65360ed20318a91beaad48d7f8
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2019-01-13 22:13:48 +0300

    MapView finished

--- Metra/Controller/BeginMoveVC.swift
+++ Metra/Controller/BeginMoveVC.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class BeginMoveVC: UIViewController {
-
+class BeginMoveVC: LocationVC {
+    
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkLocationAuthStatus()
+        mapView.delegate = self
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
     }
-    */
 
+    override func viewDidDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
+    }
+    @IBAction func locationCenterPressed(_ sender: Any) {
+    }
+ 
+}
+
+extension BeginMoveVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
 }
