0462786ee447c5756eb6786923fad836e157fc20
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2019-01-26 21:52:08 +0300

    Add UnityVC

---
+++ Metra/Controller/UnityVC.swift
@@ -0,0 +1,26 @@
+//
+//  UnityVC.swift
+//  Metra
+//
+//  Created by Zelal-Ezaldeen on 1/26/19.
+//  Copyright © 2019 Metra Company. All rights reserved.
+//
+
+import UIKit
+
+class UnityVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func startUnityBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func stopUnityBtnPressed(_ sender: Any) {
+    }
+    
+}
